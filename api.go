41adf847f2aa945483cc5ac835c57d8a96db45aa
Deivison Rocha <deivison.rocha@mercadolivre.com>
2023-02-02 18:06:39 -0300

    parte 02

--- api.go
+++ api.go
@@ -1,26 +1,45 @@
 package main
 
 import (
-
-	//"net/http"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
 
 	g "github.com/gin-gonic/gin"
 )
 
-type Data struct {
-	Data string `json:"data"`
+type Coin struct {
+	ID      string `json:"id"`
+	Content struct {
+		Price    float64 `json:"price"`
+		Currency string  `json:"currency"`
+	} `json:"content"`
+	Partial bool `json:"partial"`
 }
 
 func main() {
-
 	r := g.Default()
-	r.GET("myapi", func(ctx *g.Context) {
-		dados := Data{
-			Data: "hello",
+
+	r.GET("/myapi", func(ctx *g.Context) {
+		res, err := http.Get("https://www.coingecko.com/es/api ")
+		if err != nil {
+			ctx.JSON(http.StatusPartialContent, Coin{Partial: true})
+			return
 		}
-		ctx.JSON(200, g.H{
-			"dados": dados.Data,
-		})
+
+		body := &bytes.Buffer{}
+		_, err = io.Copy(body, res.Body)
+		if err != nil {
+			ctx.JSON(http.StatusPartialContent, Coin{Partial: true})
+			return
+		}
+
+		var result Coin
+		json.Unmarshal(body.Bytes(), &result)
+
+		ctx.JSON(http.StatusOK, result)
 	})
+
 	r.Run()
 }
